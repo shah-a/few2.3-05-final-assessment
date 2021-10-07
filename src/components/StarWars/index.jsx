8771a40f7760ab2aa12e2f2c34e0a916345b5997
Ali Shah <s.ashah@outlook.com>
2021-10-07 02:44:23 -0400

    Add: fetched characters can be saved to list in state

--- src/components/StarWars/index.jsx
+++ src/components/StarWars/index.jsx
@@ -3,29 +3,54 @@ import { fetchCharacter } from '../../functions';
 
 function StarWars() {
   const [id, setId] = useState(1);
-  const [name, setName] = useState(null);
+  const [data, setData] = useState(null);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = async () => {
+    if (id === 17) {
+      return alert('17 is invalid. Sorry!');
+    }
+
+    const [res, err] = await fetchCharacter(id);
+    if (err) {
+      return alert(err);
+    }
+
+    return setData(res);
+  };
+
+  const handleSave = () => {
+    // Clunky-looking syntax, but it's necesssary
+    // for setting `list` without duplicating entries
+    setList([...new Set([...list, data])]);
+  };
 
   return (
     <div className="StarWars">
       <form
         onSubmit={async (e) => {
           e.preventDefault();
-          if (id === 17) return alert('17 is invalid. Sorry!');
-          const [data, err] = await fetchCharacter(id);
-          if (err) return alert(err);
-          return setName(data.name);
+          handleSubmit();
         }}
       >
-        <input
-          type="number"
-          min="1"
-          max="83"
-          value={id}
-          onChange={(e) => setId(parseInt(e.target.value, 10))}
-        />
+        <label>
+          <h3>Enter SWAPI Character ID:</h3>
+          <input
+            type="number"
+            min="1"
+            max="83"
+            value={id}
+            onChange={(e) => setId(parseInt(e.target.value, 10))}
+          />
+        </label>
         <button type="submit">Submit</button>
       </form>
-      <h1>{name}</h1>
+      <h1>{data && data.name}</h1>
+      <h2>{data && `Height: ${data.height}`}</h2>
+      <h2>{data && `Mass: ${data.mass}`}</h2>
+      <h2>{data && `Hair Colour: ${data.hair_color}`}</h2>
+      <h2>{data && `Eye Colour: ${data.eye_color}`}</h2>
+      {data && <button type="button" onClick={handleSave}>Save</button>}
     </div>
   );
 }
