ea2eb568fe79f947d99311e6796632b8e2b9883c
Ali Shah <s.ashah@outlook.com>
2021-10-07 03:27:28 -0400

    Removed: `Temp` import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,12 @@
 import './App.css';
 import Home from './Home';
 import StarWars from './StarWars';
-// import Temp from './Temp';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <StarWars />
-      {/* <Temp /> */}
     </div>
   );
 }
